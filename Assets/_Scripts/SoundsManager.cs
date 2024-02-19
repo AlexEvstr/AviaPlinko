00c3e7c796a16b8bf289d75174ab45a249c241b2
Alexander Evstratov <alexdrugev@me.com>
2024-02-19 12:38:39 +0300

    Add sound manager and background music.

--- Assets/_Scripts/SoundsManager.cs
+++ Assets/_Scripts/SoundsManager.cs
@@ -1,18 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _on;
+    [SerializeField] private GameObject _off;
+
+    private void OnEnable()
     {
-        
+        LoadStatus();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LoadStatus()
     {
-        
+        int status = PlayerPrefs.GetInt("sound", 0);
+        if (status == 0)
+        {
+            SoundOn();
+        }
+        else
+        {
+            SoundOff();
+        }
+    }
+
+    public void SoundOn()
+    {
+        _off.SetActive(false);
+        _on.SetActive(true);
+        AudioListener.volume = 1;
+    }
+
+    public void SoundOff()
+    {
+        _on.SetActive(false);
+        _off.SetActive(true);
+        AudioListener.volume = 0;
+    }
+
+    private void OnDisable()
+    {
+        SaveStatus();
+    }
+
+    private void SaveStatus()
+    {
+        if (_on.activeInHierarchy)
+        {
+            PlayerPrefs.SetInt("sound", 0);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("sound", 1);
+        }
     }
 }
