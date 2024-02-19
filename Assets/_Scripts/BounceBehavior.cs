839b5d4245b50a91562c8dbe5eabf3eeccc34048
Alexander Evstratov <alexdrugev@me.com>
2024-02-19 14:22:53 +0300

    Add sounds for gameplay.

--- Assets/_Scripts/BounceBehavior.cs
+++ Assets/_Scripts/BounceBehavior.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class BounceBehavior : MonoBehaviour
 {
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _audioClip;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -14,6 +16,7 @@ public class BounceBehavior : MonoBehaviour
 
     private IEnumerator ChangeSize()
     {
+        _audioSource.PlayOneShot(_audioClip);
         transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
         yield return new WaitForSeconds(0.1f);
         transform.localScale = new Vector3(0.15f, 0.15f, 0.15f);
--- Assets/_Scripts/EndTrigger.cs
+++ Assets/_Scripts/EndTrigger.cs
@@ -2,35 +2,52 @@ using UnityEngine;
 
 public class EndTrigger : MonoBehaviour
 {
+    [SerializeField] private AudioClip _endClip;
+    [SerializeField] private AudioClip _endClipBad;
+
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GameObject.FindGameObjectWithTag("Manager").GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         string countOfPoints = collision.gameObject.tag;
         if (countOfPoints == "0")
         {
             Destroy(gameObject);
+            _audioSource.PlayOneShot(_endClipBad);
+
         }
         else if (countOfPoints == "5")
         {
+            _audioSource.PlayOneShot(_endClip);
             Destroy(gameObject);
             GameData.score += 5;
         }
         else if (countOfPoints == "10")
         {
+            _audioSource.PlayOneShot(_endClip);
             Destroy(gameObject);
             GameData.score += 10;
         }
         else if (countOfPoints == "25")
         {
+            _audioSource.PlayOneShot(_endClip);
             Destroy(gameObject);
             GameData.score += 25;
         }
         else if (countOfPoints == "50")
         {
+            _audioSource.PlayOneShot(_endClip);
             Destroy(gameObject);
             GameData.score += 50;
         }
         else if (countOfPoints == "100")
         {
+            _audioSource.PlayOneShot(_endClip);
             Destroy(gameObject);
             GameData.score += 100;
         }
@@ -40,6 +57,7 @@ public class EndTrigger : MonoBehaviour
     {
         if (transform.position.x < -3 || transform.position.x > 3 || transform.position.y < -7)
         {
+            _audioSource.PlayOneShot(_endClipBad);
             Destroy(gameObject);
         }
     }
